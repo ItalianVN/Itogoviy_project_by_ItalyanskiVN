d1979f43a4820e2e5cc6b51aec78d90776474bee
Vyacheslav <elik@example.com>
2022-11-27 17:00:30 +0700

    Modificate programm result

--- task/Program.cs
+++ task/Program.cs
@@ -1,6 +1,15 @@
-﻿// Формирование нового массива
-string[] array1 = new string[5] {"12", "123", "helloWorld", "1234", "end"};
+﻿// Ввод исходного массива с клавиатуры
+string[] GetArray(int m)
+{
+    string[] array = new string[m];
+    for (int i = 0; i < array.Length; i++)
+    {
 
+        Console.Write($"Введите {i + 1} строку:  ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
 
 // Метод вывода массива на экран
 void PrintArray(string[] array)
@@ -13,20 +22,23 @@ void PrintArray(string[] array)
 }
 
 // Метод выделения из массива строк, строки с размерностью менее 3х
-string [] NewArray(string[] array1)
+string[] NewArray(string[] array1)
 {
     string[] array2 = new string[array1.Length];
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+        if (array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        count++;
+            array2[count] = array1[i];
+            count++;
         }
     }
     return array2;
 }
 
+string[] array1 = GetArray(5);
 string[] array2 = NewArray(array1);
+Console.Write($"Готовый массив, где длина строки менее или равна трем:");
+Console.WriteLine();
 PrintArray(array2);
