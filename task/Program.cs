4bfb4c9f0336c97049f6f68e92239a6b4129166c
Vyacheslav <elik@example.com>
2022-11-27 16:04:47 +0700

    Made metod PrintArray

--- task/Program.cs
+++ task/Program.cs
@@ -1,3 +1,14 @@
-﻿//Формирование новых массивов (входного и выходного)
+﻿// Формирование новых массивов (входного и выходного)
 string[] array1 = new string[5] {"444", "23", "helloWorld", "dsa", "332"};
 string[] array2 = new string[array1.Length];
+
+// Метод вывода массива на экран
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
