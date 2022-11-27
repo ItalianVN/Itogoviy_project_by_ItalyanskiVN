4d3839735caffa1fc2fa08bae437c2f1e692b08a
Vyacheslav <elik@example.com>
2022-11-27 16:10:04 +0700

    Made metod NewArray

--- task/Program.cs
+++ task/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Формирование новых массивов (входного и выходного)
-string[] array1 = new string[5] {"444", "23", "helloWorld", "dsa", "332"};
+string[] array1 = new string[5] {"12", "123", "helloWorld", "1234", "end"};
 string[] array2 = new string[array1.Length];
 
 // Метод вывода массива на экран
@@ -12,3 +12,16 @@ void PrintArray(string[] array)
     Console.WriteLine();
 }
 
+// Метод выделения из массива строк, строки с размерностью менее 3х
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
